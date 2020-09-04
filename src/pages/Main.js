4244017f56e959dd1df37ae27252eb7454a3ca3c
karn21 <karn212000@gmail.com>
2020-09-04 16:11:39 +0530

    add main page

---
+++ src/pages/Main.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div className="container">
+      <div className="header">
+        <p>
+          <i className="fa fa-lg fa-angle-left" aria-hidden="true"></i> Your
+          Rewards
+        </p>
+      </div>
+      <div className="spinner"></div>
+      <div className="cta">
+        <h3>Spin the wheel now to get rewarded</h3>
+        <p>
+          Tap on Spin or rotate the wheel anti-clockwise and release to start
+          spinning
+        </p>
+      </div>
+      <div className="footer">
+        <h4>
+          Have a question? <a href="http://karan.codes">Get help</a>
+        </h4>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
